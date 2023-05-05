f46f158b6248165f06975162d8bee3c4b59a4978
myrajarenga <myrajarenga1234@gmail.com>
2023-05-05 02:44:33 -0700

    javascript to fetch an api request

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+  const amenities = {};
+  $('input[type="checkbox"]').change(function() {
+    if ($(this).is(':checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenities).join(', '));
+  });
+fetch('http://0.0.0.0:5001/api/v1/status/')
+  .then(response => {
+    if (response.ok) {
+      $('#api_status').addClass('available');
+      console.log(response.status);
+    } else {
+      $('#api_status').removeClass('available');
+      console.log(response.status);
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching API status:', error);
+  });
+});
+
